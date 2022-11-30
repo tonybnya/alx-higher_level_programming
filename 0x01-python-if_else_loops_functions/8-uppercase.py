de104ccd0a320452987ccca14439ed90c810b8fd
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-11-30 15:37:43 -0800

    Add: 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    new = ""
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            new += chr(ord(ch) - 32)
+        else:
+            new += ch
+
+    print(new)
