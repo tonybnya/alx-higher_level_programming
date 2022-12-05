975f1c163f0cfacd2028e068baf649dd95b4c606
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-05 03:42:39 -0800

    Update: 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     def element_at(my_list, idx):
-        if idx < 0 and idx > len(my_list):
-            return None
+        if idx < 0 or idx > len(my_list) - 1:
+            return (None)
 
         return (my_list[idx])
