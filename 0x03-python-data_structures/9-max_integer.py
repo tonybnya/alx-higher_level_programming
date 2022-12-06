27074503d8004840f8e50e18d137866ffaeb1439
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-06 14:01:31 -0800

    Add: 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        max_value =  my_list[0]
+        for val in my_list[1:]:
+            if val > max_value:
+                max_value = val
+        return (max_value)
+    return(None)
