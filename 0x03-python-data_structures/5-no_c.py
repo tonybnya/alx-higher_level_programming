beda887608353e8ada1262bee1fc4d25eb264edf
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-05 23:29:58 -0800

    Update: 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     if my_string:
         new = ""
         for ch in my_string:
-            if ch != 'c' and ch != 'C':
+            if ch != 'c' or ch != 'C':
                 new += ch
         return (new)
