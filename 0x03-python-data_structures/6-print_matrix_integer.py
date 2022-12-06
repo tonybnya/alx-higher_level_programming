59e2c75080fbad6435e4cb21ec5686d44dbd73d4
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-06 00:04:21 -0800

    Add: 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for val in row:
+                print("{:d}".format(val), end=" ")
+            print()
