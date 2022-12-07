d4871938c21946e0642e7dbd139a3ae7ad48a941
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-07 08:50:50 -0800

    Update: 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    num = 0
-    list_keys = list(a_dictionary.keys())
-    for key in list_keys:
-        num += 1
-    return (num) 
+    return (len(list(a_dictionary.keys())))
