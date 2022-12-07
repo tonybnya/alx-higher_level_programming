f753d99fe1e50a44ca35adb817b00fb3ab4c720d
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-07 06:08:46 -0800

    Update: 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = []
-    for row in matrix:
-        for val in row:
-            new.append(val ** 2)
-    return (new)
+    if matrix:
+        new = []
+        for row in matrix:
+            for val in row:
+                new.append(val ** 2)
+        return (new)
