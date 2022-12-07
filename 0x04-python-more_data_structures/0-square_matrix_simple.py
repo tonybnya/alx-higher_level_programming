b3eff605ee8d3b46fff9a0715ed22c237844b02b
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-07 06:28:55 -0800

    Update: 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    func = lambda x: x ** 2
-    new = [[func(val) for val in row] for row in matrix]
-    return (new)
+    func = lambda val: val ** 2
+    return ([[func(val) for val in row] for row in matrix])
