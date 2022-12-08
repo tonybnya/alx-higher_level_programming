85c71f432e8b5d30509782c8419c4d90c921bdec
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-08 08:01:05 -0800

    Update: 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     if not isinstance(roman_string, str):
         return (0)
@@ -9,17 +9,17 @@ def roman_to_int(roman_string):
         return (0)
 
     if len(roman_string) == 1:
-        return (numerals[roman_string])
+        return (nums[roman_string])
 
     number = 0
     prev = number
 
     for ch in roman_string[::-1]:
-        val = numerals[ch]
-        if val >= prev:
-            number += val
-            prev = val
+        value = nums[ch]
+        if value >= prev:
+            number += value
+            prev = value
         else:
-            number -= val
+            number -= value
 
     return (number)
