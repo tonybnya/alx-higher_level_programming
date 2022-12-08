6c4ff1ffb8fd0f2680165b0014b81a7b17bb91c6
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-08 08:15:54 -0800

    Update: 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,9 @@
 def weight_average(my_list=[]):
     score = 0
     weight = 0
-
-    for item in my_list:
-        score += item[0] * item[1]
-        weight += item[-1]
+    if my_list:
+        for item in my_list:
+            score += item[0] * item[1]
+            weight += item[-1]
 
     return (score / weight)
