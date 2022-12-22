c4acf60cdb24bdc04b9a38229508dcfffb59e6ca
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-22 02:21:18 -0800

    Update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
+    length = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
+            length += 1
         except IndexError:
-            print("Index Error")
+            break
+    print()
+
+    return (length)
