14d424cdb612b4bfecc4b107e38473ffee30e073
Tony <nya.tony2010@gmail.com>
2022-12-24 17:11:41 +0100

    Update files

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""class representing a square with a private instance attribute size."""
+
+
+class Square:
+    """Modelize a square."""
+    def __init__(self, size=0):
+        """
+        Initialize the private instance attribute size.
+        Args:
+            size (int): the size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        return (self.__size ** 2)
