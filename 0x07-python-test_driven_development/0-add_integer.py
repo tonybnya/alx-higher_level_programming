39e8dfcf2c8cff72af97e620c8f3b6e41644622a
Tony <nya.tony2010@gmail.com>
2023-01-02 13:27:04 +0100

    Update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 0-add_integer.py
+# tonybnya
 """
 This module performs the addition of two integers.
 """
@@ -13,10 +15,10 @@ def add_integer(a, b=98):
         b (int): another integer - default value is 98
 
     Returns:
-        The sum of the two integers.
+        The integer addition of a and b.
     """
-    return a + b
-
-
-if __name__ == '__main__':
-    print(add_integer.__doc__)
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if (not isinstance(b, int) and not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
