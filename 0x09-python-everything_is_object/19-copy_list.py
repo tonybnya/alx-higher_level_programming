ddfc733d5e459a3f62705ddba78b0698c7646d47
Tony <nya.tony2010@gmail.com>
2023-01-04 19:21:15 +0100

    update

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 19-copy_list.py
+# tonybnya
+"""
+Copy a list object
+"""
+
+
+def copy_list(l):
+    """
+    Function that returns a copy of a list
+    Arg:
+        l (list): a list as parameter
+    Returns:
+        a copy of l
+    """
+    return l[:]
