22a8626f02e7e1aba3a5fc1d0292d7a5ca645702
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-01-04 10:42:16 -0800

    update

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,17 +1,3 @@
 #!/usr/bin/python3
-# 19-copy_list.py
-# tonybnya
-"""
-Copy a list object
-"""
-
-
 def copy_list(l):
-    """
-    Function that returns a copy of a list
-    Arg:
-        l (list): a list as parameter
-    Returns:
-        a copy of l
-    """
     return l.copy()
