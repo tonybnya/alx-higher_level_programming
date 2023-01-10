b767d256176f9a2f88dfd748ee484e48155f8a01
Tony <nya.tony2010@gmail.com>
2023-01-10 23:10:54 +0100

    update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 0-read_file.py
+# tonybnya
+"""
+Reads a text file encoded with the standard encoding mode UTF-8.
+"""
+
+
+def read_file(filename=""):
+    """
+    Function that reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (text file): the path to the filename
+    """
+    with open(filename, encoding="utf-8") as file:
+        content = file.read()
+
+    print(content)
