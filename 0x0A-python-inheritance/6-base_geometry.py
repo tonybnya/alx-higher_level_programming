f24adf51ceb80c801d6138324d250491f601edfd
Tony <nya.tony2010@gmail.com>
2023-01-10 20:00:01 +0100

    update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry based on 5-base_geometry.py
+"""
+
+
+class BaseGeometry(object):
+    """
+    Class BaseGeometry with an public instance method.
+    """
+    def area(self):
+        """
+        Public instance method not implemented yet.
+        """
+        raise Exception("area() is not implemented")
