4524b8ebb28ccaf618074055b7509a53d7029dae
Tony <nya.tony2010@gmail.com>
2023-01-10 21:21:03 +0100

    update

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,14 +7,21 @@ MyInt is a rebel Class.
 
 
 class MyInt(int):
+    """
+    Class MyInt that inherits from the Python's built-in Class int.
+    MyInt is a rebel Class.
+    == and != are inverted.
+    """
     def __eq__(self, other):
         """
         Inverted equivalence.
         """
-        return int.__ne__(self, other)
+        # return int.__ne__(self, other)
+        return type(self) == type(other)
 
     def __ne__(self, other):
         """
         Inverted difference.
         """
-        return int.__eq__(self, other)
+        # return int.__eq__(self, other)
+        return type(self) != type(other)
