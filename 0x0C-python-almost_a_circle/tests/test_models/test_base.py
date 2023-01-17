66d59795b453a3ea1c8871adf14bb9433344acb5
Tony <nya.tony2010@gmail.com>
2023-01-17 07:49:27 +0100

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -12,18 +12,38 @@ class TestBase(unittest.TestCase):
     def setUp(self):
         self.base = Base()
 
-    def test_id_assignment(self):
-        """ Test for id assignment. """
-        self.assertEqual(self.base.id, 2)
-
     def test_id_assignment_with_custom_value(self):
         """ Test for id assignment with custom value. """
-        base_2 = Base(10)
-        self.assertEqual(base_2.id, 10)
+        base = Base(1)
+        self.assertEqual(base.id, 1)
 
-    def test_class_attribute_nb_objects(self):
+    def test_id_assignment_with_default_value(self):
+        """ Test for id assignment with default value. """
+        base = Base()
+        self.assertEqual(base.id, 1)
+
+    def test_private_class_attribute_nb_objects(self):
         """ Test for private class attribute __nb_objects. """
-        self.assertEqual(Base.__nb_objects, 1)
+        base_1 = Base()
+        base_2 = Base()
+        base_3 = Base()
+        self.assertEqual(base_1.id, 1)
+        self.assertEqual(base_2.id, 2)
+        self.assertEqual(base_3.id, 3)
+
+    def test_private_class_attribute_nb_objects_mixed(self):
+        """ Test for private class attribute __nb_objects and id assigment. """
+        base_1 = Base()
+        base_2 = Base(7)
+        base_3 = Base()
+        self.assertEqual(base_1.id, 1)
+        self.assertEqual(base_2.id, 7)
+        self.assertEqual(base_3.id, 2)
+
+    def test_number_of_arguments(self):
+        """ Test for number of arguments passing in the instanciation. """
+        with self.assertRaises(TypeError):
+            base = Base(1, 2)
 
 
 if __name__ == '__main__':
