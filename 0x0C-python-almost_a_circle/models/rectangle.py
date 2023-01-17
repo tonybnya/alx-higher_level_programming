e32f222cf4fcd7aff190f108678dfa76484e1648
Tony <nya.tony2010@gmail.com>
2023-01-17 08:33:36 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,4 +6,35 @@ This class has:
     - private instance attribute height
     - private instance attribute x
     - private instance attribute y
+    - class constructor def __init__(self, width, height, x=0, y=0, id=None):
 """
+Base = __import__('base').Base
+
+
+class Rectangle(Base):
+    """
+    Python Inheritance.
+    Class Rectangle inherits from Base Class.
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialize private instance attributes. """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+#     @property
+#     def width(self):
+#         """ width getter """
+#         return self.__width
+
+#     @width.setter
+#     def width(self, value):
+#         """ width setter """
+#         if type(value) is not int:
+#             raise TypeError('width must be an integer')
+
+#         if value <= 0:
+#             raise ValueError('width must be > 0')
+
+#         self.__width = value
