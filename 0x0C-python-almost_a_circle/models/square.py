0c1699335bc064477ffc7c34c21731c630caad20
Tony <nya.tony2010@gmail.com>
2023-01-18 13:12:29 +0100

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,7 +20,17 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ Initialize instances from attributes of Rectangle class. """
         super().__init__(size, size, x, y, id)
-        self.size = size
+
+    @property
+    def size(self):
+        """ size getter """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ size setter """
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Special method to print a custom string of a square object."""
