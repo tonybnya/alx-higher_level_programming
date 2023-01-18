823a0823319b6a507d115f38fdeb66cb5719211d
Tony <nya.tony2010@gmail.com>
2023-01-18 18:31:11 +0100

    update

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -15,31 +15,33 @@ def append_write(filename="", text=""):
         filename (text file): the path to the filename
         text (string): the content to insert to the file
     """
-    with open(filename, mode='a+', encoding='utf-8') as file:
-        contents = file.readlines()
-        lines = len(contents)
-        chars_added = 0
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
+    # with open(filename, mode='a+', encoding='utf-8') as file:
+    #     contents = file.readlines()
+    #     lines = len(contents)
+    #     chars_added = 0
 
-        if lines == 0:
-            file.write(text)
-            words = file.readlines()
+    #     if lines == 0:
+    #         file.write(text)
+    #         words = file.readlines()
 
-            for word in words:
-                for _ in word:
-                    chars_added += 1
-        else:
-            line_num = lines + 1
-            while line_num > lines:
-                line = file.readline()
-                words = line.strip()
+    #         for word in words:
+    #             for _ in word:
+    #                 chars_added += 1
+    #     else:
+    #         line_num = lines + 1
+    #         while line_num > lines:
+    #             line = file.readline()
+    #             words = line.strip()
 
-                for word in words:
-                    for _ in word:
-                        chars_added += 1
+    #             for word in words:
+    #                 for _ in word:
+    #                     chars_added += 1
 
-                if not line:
-                    break
+    #             if not line:
+    #                 break
 
-                lines += 1
+    #             lines += 1
 
-    return chars_added
+    # return chars_added
