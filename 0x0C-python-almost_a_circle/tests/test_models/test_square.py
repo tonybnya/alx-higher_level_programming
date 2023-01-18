ec5a0a53845ee488b25b1e80657863d8fb464b92
Tony <nya.tony2010@gmail.com>
2023-01-18 12:32:29 +0100

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -8,3 +8,21 @@ from io import StringIO
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """ Test cases for Square class. """
+
+    def setUp(self):
+        """ Method called before each test. """
+        Base._Base__nb_objects = 0
+
+    def test_square_with_default_values(self):
+        """ Test with default values. """
+        square = Square(7)
+        self.assertEqual(square.size, 7)
+        self.assertEqual(square.width, 7)
+        self.assertEqual(square.height, 7)
+        self.assertEqual(square.x, 0)
+        self.assertEqual(square.y, 0)
+        self.assertEqual(square.id, 1)
