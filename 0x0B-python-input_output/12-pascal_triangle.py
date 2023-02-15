885eca7f6185b1781aa4c8acacbc1388f131539f
Tony <nya.tony2010@gmail.com>
2023-02-15 12:17:29 +0100

    update

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# 12-pascal_triangle.py
+# tonybnya
+"""
+Technical Interview Preparation
+Pascal's Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Function that returns a list of list of integers
+    representing the Pascal's triangle of n
+
+    Arg:
+        n (int): integer always greater than 0
+    """
+    if n <= 0:
+        return []
+
+    pass
