137ea3499b2f9b9abf3a084d21977f15d53ad816
Tony <nya.tony2010@gmail.com>
2023-02-15 15:34:00 +0100

    update

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS second_table (
 id INT,
 name VARCHAR(256),
 score INT);
-
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex" 3);
 INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
