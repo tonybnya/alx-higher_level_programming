cce1c9126841332ba5ad2c1280e1fd2367733d73
Tony <nya.tony2010@gmail.com>
2023-02-15 16:40:42 +0100

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- 0. My privileges!
 -- Query
-SELECT * FROM mysql.user
-WHERE User in ('user_0d_1', 'user_0d_2')
-AND Host = 'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
