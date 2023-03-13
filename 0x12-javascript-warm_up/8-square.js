c9ebaf710256bfc0f1ad92578a19d00467e70e8a
Tony <nya.tony2010@gmail.com>
2023-03-13 17:47:22 +0100

    Update: 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,8 +7,10 @@ if (isNaN(size)) {
   log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
+    let line = '';
     for (let j = 0; j < size; j++) {
-      log(`${char}`);
+      line += `${char}`;
     }
+    log(`${line}`);
   }
 }
