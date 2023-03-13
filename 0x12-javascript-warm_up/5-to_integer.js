f153595e917920ad3cccdf5985c2208593b11598
Tony <nya.tony2010@gmail.com>
2023-03-13 17:17:48 +0100

    Update: 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const arg = process.argv[2];
 const log = (arg) => console.log(arg);
-let str = 'My number:';
-let num = parseInt(arg);
+const str = 'My number:';
+const num = parseInt(arg);
 
 if (isNaN(num)) {
   log('Not a number');
