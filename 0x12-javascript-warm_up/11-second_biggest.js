4ec7fad2a128f20cf15a0e84ee49c36e3ddeadd0
Tony <nya.tony2010@gmail.com>
2023-03-13 18:41:23 +0100

    Update: 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const log = (arg) => console.log(arg);
-const args = process.argv.length;
+const args = process.argv;
 
 function secondBiggest (arr) {
   return arr[arr.length - 2];
 }
 
-if (args <= 3) {
+if (args.length <= 3) {
   log(0);
 } else {
   args.sort();
