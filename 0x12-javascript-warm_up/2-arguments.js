b1c4745951dc3c828b905985285ac7857ca27791
Tony <nya.tony2010@gmail.com>
2023-03-13 14:36:42 +0100

    Update: 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,5 @@ let str = 'Argument';
 
 if (args === 2) {
   console.log('No argument');
-} else {
-  args === 3 ? console.log(`${str} found`) : console.log(`${str}s found`);
 }
+args === 3 ? console.log(`${str} found`) : console.log(`${str}s found`);
