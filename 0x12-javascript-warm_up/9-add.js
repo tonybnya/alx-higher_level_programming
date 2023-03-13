01361ec7fe614e1d5d2f92057fbea80999cbee4b
Tony <nya.tony2010@gmail.com>
2023-03-13 18:01:45 +0100

    Update: 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const log = (arg) => console.log(arg);
-const a = process.argv[2];
-const b = process.argv[3];
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
 function add(a, b) {
   return a + b;
 }
 
-log(a === 'undefined' || b === 'undefined' ? NaN : parseInt(add(a, b)));
+log(a === 'undefined' || b === 'undefined' ? NaN : add(a, b));
