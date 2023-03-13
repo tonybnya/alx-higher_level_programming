657673124f3dc7fee5f039cd533507a0cc044dd8
Tony <nya.tony2010@gmail.com>
2023-03-13 15:14:22 +0100

    Update: 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+const arg = process.argv[2];
+const log = (arg) => console.log(arg);
+log(typeof arg === 'undefined' ? 'No argument' : arg);
