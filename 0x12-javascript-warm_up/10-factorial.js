d904c19366b511b31113f8638f913006f9d98845
Tony <nya.tony2010@gmail.com>
2023-03-13 18:09:27 +0100

    Add: 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const log = (arg) => console.log(arg);
+const arg = parseInt(process.argv[2]);
+
+function factorial (num) {
+  if (num === 1) {
+    return 0;
+  }
+
+  return factorial(num) * factorial(num - 1);
+}
+
+log(arg === 'undefined' ? 1 : factorial(arg))
