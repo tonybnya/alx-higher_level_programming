b059bb021d8d7499e57295555ee3c717d698ec56
Tony <nya.tony2010@gmail.com>
2023-03-13 16:52:51 +0100

    Update: 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.argv;
+const args = process.argv;
 const log = (arg) => console.log(arg);
 const str = 'is';
+const arg2 = args[2];
+const arg3 = args[3];
 
 log(`${args[2]} ${str} ${args[3]}`);
