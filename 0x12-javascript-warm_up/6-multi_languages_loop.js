8e0ba8261b28620fab9783b1a37d2049ffa87801
Tony <nya.tony2010@gmail.com>
2023-03-13 17:25:38 +0100

    Add: 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const log = (arg) => console.log(arg);
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < languages.length; i++) {
+  log(languages[i]);
+}
