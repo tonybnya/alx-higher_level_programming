04c7890c17a3ca189e7c2300267383dfa2eb2f23
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-03-13 06:24:14 -0700

    Update: 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,7 +6,7 @@ const log = (arg) => console.log(arg);
 let str = 'Argument';
 
 if (args === 2) {
-  log('No argument');
+  console.log('No argument');
 } else {
-  args === 3 ? log(`${str} found`): log(`${str}s found`);
+  args === 3 ? console.log(`${str} found`): console.log(`${str}s found`);
 }
