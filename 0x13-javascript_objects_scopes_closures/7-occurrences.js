d63b2ba236147269f4da94392a2fad8c0e8626c6
Tony <nya.tony2010@gmail.com>
2023-03-14 15:09:46 +0100

    Add: 7-main.js 7-square.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce((counter, current) => {
+    current === searchElement ? counter + 1 : counter, 0
+  });
+};
