123dcd7fdab87ce64d1348c223392680f164fc6f
Tony <nya.tony2010@gmail.com>
2023-03-14 16:02:55 +0100

    Update: 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,13 +2,12 @@
 const { dict } = require('./101-data').dict;
 let result = {};
 
-for (const key in dict) {
-  const occurrences = dict[key];
-
-  if (!result[occurrences]) {
-    result[occurrences] = [];
+for (let key in dict) {
+  if (result[dict[key]] === undefined) {
+    result[dict[key]] = [key];
+  } else {
+    result[dict[key]].push(key);
   }
-  result[occurrences].push(key);
 }
 
 console.log(result);
