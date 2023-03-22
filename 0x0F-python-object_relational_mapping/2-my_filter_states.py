ed4182901e73a6eeddb5e523b2a694ecb9074b3f
Tony <nya.tony2010@gmail.com>
2023-03-22 16:03:24 +0100

    Update: 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,7 @@ def main():
 
     cursor = database.cursor()
     cursor.execute("SELECT * FROM states WHERE states.name \
-                   LIKE BINARY %s ORDER BY states.id", (state_name))
+                   BINARY %s ORDER BY states.id", (state_name))
 
     for state in cursor.fetchall():
         print(state)
