3bdda871d9d767e866add112857006790377c5ac
Tony <nya.tony2010@gmail.com>
2023-04-03 16:18:02 +0100

    Add: 102-relationship_cities_states_list.py

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# 102-relationship_cities_states_list.py
+# Author: @tonybnya
+"""
+This script lists all City objects from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
+
+
+def main():
+    """ Main function """
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    session = sessionmaker(bind=engine)()
+
+    for city in session.query(City).order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+
+
+if __name__ == '__main__':
+    main()
