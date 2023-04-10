54651ab1a096b6fd80e7e04ad090c883a830aa28
Tony <nya.tony2010@gmail.com>
2023-04-10 18:09:04 +0100

    Update: 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# 100-github_commits.py
+# @tonybnya
+"""
+This python script lists 10 commits (from the most recent to oldest)
+of the repository 'rails' by the user 'rails'.
+The script uses the GitHub API.
+Here is the documentation https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+"""
+import sys
+import requests
+
+
+def main():
+    """ Main function """
+    repo = sys.argv[1]
+    user = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(user, repo)
+
+    json = get(url).json()
+
+    print(json)
+
+
+if __name__ == '__main__':
+    main()
