4d2a5fb92c71bd6f98a398eb705e4bf1818b2e39
Tony <nya.tony2010@gmail.com>
2023-05-01 13:02:59 +0100

    Update: 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,11 +4,9 @@ const fs = require('fs');
 const file = process.argv[2];
 const content = process.argv[3];
 
-if (process.argv === 4) {
-  fs.writeFile(file, content, 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-  });
-}
+fs.writeFile(file, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
