123b5ef16f7f63d527f75047aac53d9f6dfe8b0b
Tony <nya.tony2010@gmail.com>
2023-05-01 14:01:20 +0100

    Update: 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,8 +3,8 @@
 
 const req = require('request');
 const fs = require('fs');
-const url = process.argv[2]
-const file = process.argv[3]
+const url = process.argv[2];
+const file = process.argv[3];
 
 req.get(url, (err, res, body) => {
   if (err) {
