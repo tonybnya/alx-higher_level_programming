f853dede044d7213d18930323f5e41e69f7e4c60
tonybnya <nya.tony2010@gmail.com>
2023-05-01 04:49:04 -0700

    Add: 1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+# This JS script writes a string to a file.
+
+const fs = require('fs');
+const file = process.argv[2];
+const content = process.argv[3];
+
+if (process.argv === 4) {
+  fs.writeFile(file, content, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  }); 
+}
