4001afaf0f45632437f9a867a30defd94a195517
Tony <nya.tony2010@gmail.com>
2023-05-01 12:26:50 +0100

    Add: 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const log = (arg) => console.log(arg);
+
+if (process.argv.length === 3) {
+  fs.readFile(file, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    log(data);
+  });
+}
