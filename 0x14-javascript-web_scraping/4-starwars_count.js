292446ec4a2f265beae6ff7e83e0b223f2f529d4
Tony <nya.tony2010@gmail.com>
2023-05-01 13:49:35 +0100

    Update: 4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// This JS script prints the number of movies where
+// the character “Wedge Antilles” is present.
+
+const req = require('request');
+const url = process.argv[2]
+const log = (arg) => console.log(arg);
+let counter = 0;
+
+req(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  const data = JSON.parse(body);
+
+  data.results.forEach((movie) => {
+    movie.characters.forEach((char) => {
+      if (char.includes(18)) {
+        counter += 1;
+      }
+    })
+  });
+
+  log(counter);
+});
