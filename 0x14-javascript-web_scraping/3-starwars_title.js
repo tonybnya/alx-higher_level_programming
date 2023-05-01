3be45e02b5eea7d80764b4890bdfb78da75a929d
Tony <nya.tony2010@gmail.com>
2023-05-01 13:25:30 +0100

    Update: 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -8,4 +8,12 @@ const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/:'
 const endpoint = `${url}${id}`;
 
-log(endpoint);
+req.get(endpoint, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  log(body);
+  log(res);
+});
