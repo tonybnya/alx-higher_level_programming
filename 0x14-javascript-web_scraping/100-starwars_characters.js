b037d284627eb07d80712b45aca4610692bb21f5
Tony <nya.tony2010@gmail.com>
2023-05-01 14:48:36 +0100

    Update: 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,6 +7,19 @@ const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 const endpoint = `${url}${id}`;
 
+const printChars = (endpoint) => {
+  req.get(endpoint, (err, res, body) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    const data = JSON.parse(body);
+
+    log(data.name);
+  });
+};
+
 req.get(endpoint, (err, res, body) => {
   if (err) {
     console.error(err);
@@ -14,6 +27,9 @@ req.get(endpoint, (err, res, body) => {
   }
 
   const data = JSON.parse(body);
-  log(data.title);
-  log(data.characters);
+  const chars = data.characters;
+
+  chars.forEach((char) => {
+    printChars(char);
+  });
 });
