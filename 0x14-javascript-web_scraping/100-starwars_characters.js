23c273d3ec3123cdd97f43703bd45da4edf418a0
Tony <nya.tony2010@gmail.com>
2023-05-01 14:40:00 +0100

    Update: 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 // This JS script prints all characters of a Star Wars movie.
 
+const log = (arg) => console.log(arg);
 const req = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/';
