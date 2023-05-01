ddeec7a22642ef5aeefcb5d6c152db4d996aef53
Tony <nya.tony2010@gmail.com>
2023-05-01 13:58:22 +0100

    Add: 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// This JS script get the contents of a webpage and stores it in a file.
+
+const req = require('request');
+const fs = require('fs');
+const url = process.argv[2]
+const file = process.argv[3]
+
+req.get(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  fs.writeFile(file, body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+});
